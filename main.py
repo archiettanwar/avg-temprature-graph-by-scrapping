aeed4f5c31c8f33f1492361430c7d1a67d9c3d4c
Archiet <archiettanwar71@gmail.com>
2025-10-02 20:02:51 +0530

    read the temp

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ import selectorlib
 
 URL="http://programmer100.pythonanywhere.com/"
 
+def scrape(url):
+    response=requests.get(url)
+    source=response.text
+    return source
+
+def extract(source):
+    extractor=selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value=extractor.extract(source)["temp"]
+    return value
+
+def add_temp
+
+if __name__=="__main__":
+    while True:
+        source=(scrape(URL))
+        extracted=extract(source)
+        print(extracted)
+    time.sleep(2)
